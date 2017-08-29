d491f96c0f27bcb800b18f61d2acee4d6e38f2c8
TimOdell <tim@theodells.org>
2017-08-29 15:23:36 -0700

    helpful code snippets

---
+++ Get-FileCounts.ps1
@@ -0,0 +1,8 @@
+﻿$rootFolder = "c:\temp"
+
+Get-ChildItem $rootFolder `
+    -Recurse -Directory | 
+        Select-Object `
+            FullName, `
+            @{Name="FileCount";Expression={(Get-ChildItem $_ -File | 
+        Measure-Object).Count }} 
---
+++ Split-ArrayIntoSlices.ps1
@@ -0,0 +1,21 @@
+﻿cls
+$ids=@(0,1,2,3,4,5,6,7,8,9)
+$size=3
+
+<# 
+Manual Selection:
+    $ids | Select-Object -First 3 -Skip 0
+    $ids | Select-Object -First 3 -Skip 3
+    $ids | Select-Object -First 3 -Skip 6
+    $ids | Select-Object -First 3 -Skip 9
+#>
+
+# Select via looping
+$idx = 0
+while ($($size * $idx) -lt $ids.Length){
+    
+    $group = $ids | Select-Object -First $size -skip ($size * $idx)
+    $group -join ","
+    $idx ++
+} 
+
